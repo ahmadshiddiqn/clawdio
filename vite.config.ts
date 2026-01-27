a8f1581e8d9512f2da8358ad1db5a01458f232e8
Ahmad Shiddiq <ahmad_shiddiq@baramultigroup.co.id>
2026-01-27 18:24:04 +0700

    fix: try fix bug in PR

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [sveltekit()],
   ssr: {
-    noExternal: ['three', '@threlte/core', '@threlte/extras']
+    noExternal: ['three', '@threlte/core', '@threlte/extras'],
+    external: ['three/examples/jsm/objects/GroundedSkybox.js']
+  },
+  resolve: {
+    alias: {
+      'three/examples/jsm/objects/GroundedSkybox.js': path.resolve(__dirname, 'src/lib/stubs/GroundedSkybox.js')
+    }
   },
   server: {
     fs: {
